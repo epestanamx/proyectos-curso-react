1b891e6befd077c61feda1a013fe5ed104a8b847
Ernesto Pestaña <epestanamx@gmail.com>
2019-09-05 15:36:23 -0500

    Proyecto 2 - Sitio de noticias

---
+++ noticias/README.md
@@ -0,0 +1,11 @@
+# Sitio de noticias
+Proyecto realizado con react, permite ver el funcionamiento basico de los componente, el uso de formularios y el consumo de API REST.
+
+Puedes ver el ejemplo en: [Sitio de noticias](https://sitio-noticias-react.netlify.com/) 
+
+### `npm start`
+Para iniciar la aplicación en mode desarrollo.<br>
+Abre [http://localhost:3000](http://localhost:3000) para ver la aplicación en el navegador.
+
+La página se volverá a cargar si realizas modificaciones.<br>
+También podrás ver los errores del linter en la consola.
---
+++ noticias/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component, Fragment } from 'react';
+import Header from './components/Header';
+import ListaNoticias from './components/ListaNoticias';
+import Formulario from './components/Formulario';
+
+class App extends Component {
+  state = {
+    noticias: []
+  }
+
+  async componentDidMount() {
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = async (categoria = 'general') => {
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&q=&apiKey=0b14afb8c1ad4f399c098ffd430f4d79`;
+
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+    this.setState({
+      noticias: noticias.articles,
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Header
+          titulo="Noticias React" />
+        
+        <div className="container white contenedor-noticias">
+          <Formulario
+            consultarNoticias={this.consultarNoticias}/>
+          <ListaNoticias
+            noticias={this.state.noticias}/>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
